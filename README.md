b0253b503bcbf389a4fbd68eb3ff28cdef7690af
Ronan <36137958+ro-stack@users.noreply.github.com>
2019-05-24 11:29:24 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,37 @@
+Ronan Hayes - Assignment Two
+HOW TO RUN:
+- Firstly run the stitchRows program.
+- This program uses 5 folders each containing 4 images, representing each 
+row in the image.
+- The output of the stitched rows will be saved to the folder stitchedRows.
+- The best version of the stitchedRows are already in there, however if you
+ run the program they will overwrite.
+- Next run the stitchAll program.
+- This uses the stitched row images from the stitchedRows folder to stitch
+ them vertically.
+- Like before, the best version of the panorama is already in the 
+finalStitches folder.
+- Another folder called best contains the current best outcome for all 
+parts.
+
+NOTE:
+- The folder locations should work as they are when running the two 
+programs, but may need to be changed if need be - see comments.
+- I have included the best versions as the stitching is not always 100% 
+(especially row2) and can mess up a lot of the time.
+
+FOLDERS & PROGRAMS:
+- stitchRows.m - run 1st to stitch the five rows of images in folders 
+row1/2/3/4/5
+- stitchAll.m - run second to stitch the 5 images created which are stored in 
+stitchedRows folder
+- stitchedRows - contains the images created by stitchRows program
+- row5 - row 5 images
+- row4 - row 4 images
+- row3 - row 3 images
+- row2 - row 2 images
+- row1 - row 1 images
+- images - all images
+- finalStitches - final panorama image created from stitchAll program
+- bestStitches - some images of the best versions of the stitched rows and 
+the final stitch
---
+++ stitchRows.m
@@ -0,0 +1,162 @@
+% Ronan Hayes - Assignment Two
+% Run first to create 5 stitched rows
+clc;
+clear all;
+close all;
+
+% For each rows folder add them to the datastore 
+for j = 1:5
+    
+    switch(j)
+        case 1
+            wallImages = imageDatastore('row1');
+        case 2
+            wallImages = imageDatastore('row2');
+        case 3
+            wallImages = imageDatastore('row3');
+        case 4
+            wallImages = imageDatastore('row4');
+        case 5
+            wallImages = imageDatastore('row5');
+    end
+
+    
+    % Read the first image from the image set.
+    I = readimage(wallImages, 1);
+    
+    % Initialize features for I(1)
+    % Detect SURF features - also tried HARRIS/MSER/BRISK
+    grayImage = rgb2gray(I);
+    points = detectSURFFeatures(grayImage);
+    [features, points] = extractFeatures(grayImage, points);
+    
+    % Initialize all the transforms to the identity matrix. Note that the
+    % projective transform is used here because the building images are fairly
+    % close to the camera. Had the scene been captured from a further distance,
+    % an affine transform would suffice.
+    
+    % Tested with affine too
+    numImages = numel(wallImages.Files(1:4));
+    tforms(numImages) = projective2d(eye(3));
+    
+    % Initialize variable to hold image sizes.
+    imageSize = zeros(numImages,2);
+    
+    % Iterate over remaining image pairs
+    for n = 2:numImages
+        
+        % Store points and features for I(n-1).
+        pointsPrevious = points;
+        featuresPrevious = features;
+        
+        % Read I(n).
+        I = readimage(wallImages, n);
+        
+        % Convert image to grayscale.
+        grayImage = rgb2gray(I);
+        
+        % Save image size.
+        imageSize(n,:) = size(grayImage);
+        
+        % Detect and extract SURF features for I(n).
+        points = detectSURFFeatures(grayImage);
+        [features, points] = extractFeatures(grayImage, points);
+        
+        % Find correspondences between I(n) and I(n-1).
+        indexPairs = matchFeatures(features, featuresPrevious, 'Unique', true);
+        
+        % if statement for only if indexPairs are greater or equal to 4
+        if length(indexPairs) >= 4
+            matchedPoints = points(indexPairs(:,1), :);
+            matchedPointsPrev = pointsPrevious(indexPairs(:,2), :);
+            
+            % Estimate the transformation between I(n) and I(n-1).
+            tforms(n) = estimateGeometricTransform(matchedPoints, matchedPointsPrev,'projective', 'Confidence', 99, 'MaxNumTrials', 2000);
+            
+            % Compute T(n) * T(n-1) * ... * T(1)
+            tforms(n).T = tforms(n).T * tforms(n-1).T;
+        end
+    end
+    
+    % Compute the output limits  for each transform
+    for i = 1:numel(tforms)
+        [xlim(i,:), ylim(i,:)] = outputLimits(tforms(i), [1 imageSize(i,2)], [1 imageSize(i,1)]);
+    end
+    
+    avgXLim = mean(xlim, 2);
+    
+    [~, idx] = sort(avgXLim);
+    
+    centerIdx = floor((numel(tforms)+1)/2);
+    
+    centerImageIdx = idx(centerIdx);
+    
+    Tinv = invert(tforms(centerImageIdx));
+    
+    for i = 1:numel(tforms)
+        tforms(i).T = tforms(i).T * Tinv.T;
+    end
+    
+    
+    for i = 1:numel(tforms)
+        [xlim(i,:), ylim(i,:)] = outputLimits(tforms(i), [1 imageSize(i,2)], [1 imageSize(i,1)]);
+    end
+    
+    maxImageSize = max(imageSize);
+    
+    % Find the minimum and maximum output limits
+    xMin = min([1; xlim(:)]);
+    xMax = max([maxImageSize(2); xlim(:)]);
+    
+    yMin = min([1; ylim(:)]);
+    yMax = max([maxImageSize(1); ylim(:)]);
+    
+    % Width and height of panorama.
+    width  = round(xMax - xMin);
+    height = round(yMax - yMin);
+    
+    % Initialize the "empty" panorama.
+    panorama = zeros([height width 3], 'like', I);
+    
+    blender = vision.AlphaBlender('Operation', 'Binary mask', ...
+        'MaskSource', 'Input port');
+    
+    % Create a 2-D spatial reference object defining the size of the panorama.
+    xLimits = [xMin xMax];
+    yLimits = [yMin yMax];
+    panoramaView = imref2d([height width], xLimits, yLimits);
+    
+    % Create the panorama.
+    for i = 1:numImages
+        
+        I = readimage(wallImages, i);
+        
+        % Transform I into the panorama.
+        warpedImage = imwarp(I, tforms(i), 'OutputView', panoramaView);
+        
+        % Generate a binary mask.
+        mask = imwarp(true(size(I,1),size(I,2)), tforms(i), 'OutputView', panoramaView);
+        
+        % Overlay the warpedImage onto the panorama.
+        panorama = step(blender, panorama, warpedImage, mask);
+    end
+    
+    % Create JPEGs for each row in stitchedRows folder
+    name = sprintf('stitchedRows/p%.0f.jpg', j);
+    imwrite(panorama, name);
+     figure,   imshow(panorama)
+end
+
+%% Limitations
+% The main limitation of this program is the final image is very poor and
+% doesnt represent the panorama image well. It also like I said in the
+% README file it is not 100% all of the time and the images in bestStitches
+% are the current best outcomes Ive had. It also requires the knowledge of
+% what images are in each row. I tried to research into ways to make it a
+% better solution such as finding the largest matchedfeatures image and
+% build from there. Or build the image from knowing the correct order, but
+% the outputs never worked. The annoying part for me is that the same
+% output is never guaranteed, and when you run it you could get a extremely
+% distorted final stitched image - that is why I included some of the
+% better versions within the bestStitches folder. If the stitchAll output
+% is majority blur or majority black re-run.
